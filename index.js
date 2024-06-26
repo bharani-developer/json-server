5371578ce1a9b0db2889a46bb8154625008cef30
Bharani karthikeyan <bharani.developer@gmail.com>
2024-06-26 16:35:45 +0530

    serve

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./worldwise/data/cities.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
